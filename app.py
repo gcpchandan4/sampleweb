644508b19164071f64335ed73afdba70fb9a4630
Chandan Kumar <chandankumar@Chandans-MacBook-Air.local>
2021-04-08 21:54:30 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf500
+{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red115\green138\blue4;\red249\green249\blue249;\red83\green83\blue83;
+\red33\green118\blue199;\red37\green146\blue134;}
+{\*\expandedcolortbl;;\cssrgb\c52157\c60000\c0;\cssrgb\c98039\c98039\c98039;\cssrgb\c40000\c40000\c40000;
+\cssrgb\c14902\c54510\c82353;\cssrgb\c16471\c63137\c59608;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl460\partightenfactor0
+
+\f0\fs32\fsmilli16200 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 from\cf4 \strokec4  flask \cf2 \strokec2 import\cf4 \strokec4  Flask\cb1 \
+\
+\pard\pardeftab720\sl460\partightenfactor0
+\cf4 \cb3 app = Flask(__name__)\cb1 \
+\
+\cb3 @app.route('/')\cb1 \
+\pard\pardeftab720\sl460\partightenfactor0
+\cf2 \cb3 \strokec2 def\cf4 \strokec4  \cf5 \strokec5 index\cf4 \strokec4 ():\cb1 \
+\pard\pardeftab720\sl460\partightenfactor0
+\cf4 \cb3     \cf2 \strokec2 return\cf4 \strokec4  \cf6 \strokec6 'Web App with Python Flask!'\cf4 \cb1 \strokec4 \
+\
+\cb3 app.run(host=\cf6 \strokec6 '0.0.0.0'\cf4 \strokec4 , port=\cf6 \strokec6 81\cf4 \strokec4 )\
+}
