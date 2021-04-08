84175116669ce3164877d46aa2011686e4b3e2a5
gcpchandan4 <76544737+gcpchandan4@users.noreply.github.com>
2021-04-08 23:44:07 +0530

    upate1

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')     
 def index():
     return 'Web App with Python Flask!'
 
-app.run(host='0.0.0.0', port=80)
+app.run(host='0.0.0.0', port=8080)
